afbae78f9de81cb6d1da7df502c1c6b7ddd52418
SantiagoJaraHernandez <167146795+SantiagoJaraHernandez@users.noreply.github.com>
2024-07-17 11:38:11 -0500

    upgrading chat view

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -124,12 +124,14 @@ const Profile = () => {
             )}
           </div>
           <div className="flex space-x-4 mt-4 sm:mt-0">
-            <button
-              className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-full"
-              onClick={() => navigate("/edit-profile")}
-            >
-              Editar Perfil
-            </button>
+            {user?._id === myId && (
+              <button
+                className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-full"
+                onClick={() => navigate("/edit-profile")}
+              >
+                Editar Perfil
+              </button>
+            )}
             {user?._id !== myId && (
               <button
                 onClick={() => handleFollowUser(user._id)}
