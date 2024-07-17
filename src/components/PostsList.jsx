2a09c0133fbe7374642eaf22ed0371fc8fbf7520
wasupalonely <juandiegomaciasoy@gmail.com>
2024-07-16 22:57:08 -0500

    solving some feed ui bugs

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -6,6 +6,8 @@ import Post from "./Post";
 import axios from "axios";
 import { toast } from "react-toastify";
 import usePosts from "../hooks/usePosts";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const PostsList = ({
   posts,
